253c40fc03527204d4fb68b894a968168e8fecae
kodjomessan <kodjo.agossou@etu.univ-smb.fr>
2025-03-28 14:57:35 +0100

    modification des commentaires

--- ecriture_ppm.cpp
+++ ecriture_ppm.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <iostream>
 
-// Fonction pour écrire une image au format PPM (type P3)
+// Fonction pour écrire une image au format PPM ( le type de P3)
 void ecrire_image_ppm(const Image& image, const std::string& nom_fichier) {
     // Ouverture du fichier en ecriture 
     std::ofstream fichier(nom_fichier);
@@ -13,7 +13,7 @@ void ecrire_image_ppm(const Image& image, const std::string& nom_fichier) {
 
     std::cout << "ecriture en cours ..." << std::endl;
 
-    // === ÉTAPE 1 : écrire l'en-tête du fichier PPM ===
+    // === ÉTAPE 1 : écriture de l'en-tête du fichier PPM ===
     fichier << "P3\n"; // P3 indique qu'on écrit une image en ASCII (et non binaire)
     fichier << image.largeur << " " << image.hauteur << "\n"; // dimensions de l'image
     fichier << image.valeur_max << "\n"; // valeur maximale pour les couleurs (ex : 255)
